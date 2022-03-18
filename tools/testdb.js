4f8028a21e54531809ca373ea4675b6ec7ce6170
Quentin Philipp <philippquentin6@gmail.com>
2022-03-18 15:51:54 +0100

    Update tool to check connection

--- tools/testdb.js
+++ tools/testdb.js
@@ -3,12 +3,16 @@
 var mysql = require('mysql2');
 require('dotenv').config()
 
-var con = mysql.createConnection({
+const config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
-});
+};
+
+var con = mysql.createConnection(config);
+
+console.log(config)
 
 con.connect(function(err) {
   if (err) throw err;
